08ce43843a0edddde4f68721788db23137eb6eb2
unknown <eljohn.bonga@g.msuiit.edu.ph>
2019-03-22 00:38:49 +0800

    login and restaurant-CrUD

--- api/__init__.py
+++ api/__init__.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
 import os
 from flask_sqlalchemy import SQLAlchemy
-
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
 
 server = Flask(__name__)
+
 db = SQLAlchemy(server)
 
+# server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 server.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-server.secret_key = os.urandom(24)
+server.secret_key = 'thisissecret'
 
 from models import *
 from app import *

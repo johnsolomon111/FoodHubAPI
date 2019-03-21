25bf363576d31a3a379c5cc4ad030257db447c2a
John Benedict Solomon <johnbenedict.solomon@g.msuiit.edu.ph>
2019-03-21 16:00:25 +0800

    Modified Models

--- api/app.py
+++ api/app.py
@@ -1,8 +1,11 @@
 from api import server
 from models import *
+from flask_restplus import Api, Resource
+
+app = Api(server)
 
 @server.route('/', methods=['GET'])
 def index():
     return '<h1>Deployed!</h1>'
 
-    
+
--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from api import db
 from werkzeug.security import generate_password_hash
 
 class Customer(db.Model):
+	__tablename__ = 'customer'
 	customer_id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(30), nullable=False, unique=True)
 	password = db.Column(db.String(80), nullable=False)
@@ -18,6 +19,7 @@ class Customer(db.Model):
 		self.contact_number = contact_number
 		self.gender = gender
 class Owner(db.Model):
+	__tablename__= 'owner'
 	owner_id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(30), nullable=False, unique=True)
 	password = db.Column(db.String(80), nullable=False)
@@ -25,6 +27,7 @@ class Owner(db.Model):
 	lastname = db.Column(db.String(50), nullable=False)
 	contact_number = db.Column(db.String(11))
 	gender = db.Column(db.String(6), nullable=False)
+	restaurants = db.relationship('Restaurant', backref='restaurant_owner')
 	
 	def __init__(self, username='', password='', firstname='', lastname='', contact_number='', gender=''):
 		self.username = username
@@ -34,4 +37,19 @@ class Owner(db.Model):
 		self.contact_number = contact_number
 		self.gender = gender
 
-db.create_all()		
+class Restaurant(db.Model):
+	__tablename__='restaurant'
+	restaurant_id = db.Column(db.Integer, primary_key=True)
+	restaurant_name = db.Column(db.String(30), nullable=False, unique=True)
+	restaurant_type = db.Column(db.String(30), nullable=False)
+	bio = db.Column(db.String(200), nullable=False)
+	locations = db.Column(db.String(200), nullable=False)
+	restaurant_owner_id = db.Column(db.Integer, db.ForeignKey('owner.owner_id'))
+
+	def __init__(self, restaurant_name='', restaurant_type='', bio='', locations=''):
+		self.restaurant_name = restaurant_name
+		self.restaurant_type = restaurant_type
+		self.bio = bio
+		self.locations = locations
+
+db.create_all()
